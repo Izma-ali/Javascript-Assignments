69d831148b05315503e36bdf94eecf958179a8a0
Izma-ali <alimuhammadsameed@gmail.com>
2024-04-19 21:07:30 +0500

    work done

---
+++ app.js
@@ -0,0 +1,25 @@
+// === Chapter 1 (Q1) === //
+
+// alert("Hi,Welcome to the page");
+
+
+// === Chapter 1 (Q2) === //
+
+// alert("Error! Please enter a valid password.");
+
+
+// === Chapter 1 (Q3) === //
+
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+
+// === Chapter 1 (Q4) === //
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+
+// === Chapter 1 (Q5) === //
+
+// I've done this task on browser's console
+
