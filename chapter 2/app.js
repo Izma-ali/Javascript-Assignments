9636d3d0aa9966dcd5c957fc22741afd0ba77b81
Izma-ali <alimuhammadsameed@gmail.com>
2024-04-23 19:55:05 +0500

    chapter 2 added

---
+++ chapter 2/app.js
@@ -0,0 +1,16 @@
+//=== chapter 2 (Q1) ===//
+
+// var username;
+
+
+//=== chapter 2 (Q2) ===//
+
+// var myName = "Izma Ali";
+// console.log (myName);
+
+
+// //=== chapter 2 (Q7) ===//
+
+// var book = "A smarter way to learn Javascript"
+// alert ("I am trying to learn from the Book " + book);
+
