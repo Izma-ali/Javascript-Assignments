b97c1267897e7dfa6d08a313175b1ce6a063ebce
Izma-ali <alimuhammadsameed@gmail.com>
2024-04-24 17:11:12 +0500

    chapter 3 added

---
+++ chapter 3/app.js
@@ -0,0 +1,10 @@
+// === Chapter 3 (Q1) === //
+
+// var age = "21 years"
+// alert ("I am "+ age +" old");
+
+
+// === Chapter 3 (Q2) === //
+
+// var noOfVisit = "6 times"
+// alert ("You have visited this site "+noOfVisit);
