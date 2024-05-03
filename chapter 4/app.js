4ac49bfeeb695921780451130bf2affed72b314c
Izma-ali <alimuhammadsameed@gmail.com>
2024-05-04 03:17:07 +0500

    chapter 4 and 5 added

---
+++ chapter 4/app.js
@@ -0,0 +1,26 @@
+// ==== Chapter 4 (Q1)====
+
+// var name , class , rollNo ;
+
+
+// ==== Chapter 4 (Q2)====
+
+// var myName , _myName , $myName ,myName12 ,my12Name ;
+
+
+// ==== Chapter 4 (Q3)====
+
+// var 2myName , my Name , alert , my-Name ,  myName@ ;
+
+
+// ==== Chapter 4 (Q1)====
+
+// document.write("<p>Variable names can only contain</p>");
+
+// document.write("<p>Variable names can only contain numbers, $ and _.For example $my_1stVariable</p>")
+
+// document.write("<p>Variables must begin with a letter, $ or _. For example $name, _name or name</p>")
+
+// document.write("<p>Variable names are case sensitive</p>")
+
+// document.write("<p>Variable names should not be JS keywords</p>")
---
+++ chapter 5/app.js
@@ -0,0 +1,45 @@
+// ==== Chapter 5 (Q1) ==== //
+
+// var num1 = 3
+// var num2 = 5
+
+// var sum = num1+num2
+
+// document.write("Sum of " + num1 +" and " + num2 + " is " + sum )
+
+
+// ==== Chapter 5 (Q2) ==== //
+    //  Subtraction   //
+// var num1 = 3
+// var num2 = 5
+
+// var subtraction = num1-num2
+
+// document.write( num1 +" minus " + num2 + " is " + subtraction )
+
+
+    //  Multiplication  //
+// var num1 = 3
+// var num2 = 5
+
+// var sum = num1*num2
+
+// document.write("Product of " + num1 +" and " + num2 + " is " + sum )
+
+
+    //  division  //
+// var num1 = 10
+// var num2 = 5
+
+// var sum = num1/num2
+
+// document.write( num1 +" divided by " + num2 + " is " + sum )
+
+
+    //    Modulus   //
+    // var num1 = 10
+    // var num2 = 5
+    
+    // var sum = num1%num2
+    
+    // document.write( num1 +" divided by " + num2 + " then " + sum + " is the remainder")
