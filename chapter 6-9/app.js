681b02cf87b7af94ffae8c21c3b47e2c5cc27617
Izma-ali <alimuhammadsameed@gmail.com>
2024-05-09 19:38:39 +0500

    chapter 6-9 added

---
+++ chapter 6-9/app.js
@@ -0,0 +1,66 @@
+// === Chapter 6-9 (Q1) === //
+
+// var a = 10
+
+// document.write("The value of ++a is: "+ ++a +"<br>")
+// document.write("Now the value of a is: "+a+"<br>"+"<br>")
+
+
+// document.write("The value of a++ is: "+ a++ +"<br>")
+// document.write("Now the value of a is: "+ a +"<br>"+"<br>")
+
+
+// document.write("The value of --a is: "+ --a +"<br>")
+// document.write("Now the value of a is: "+ a +"<br>"+"<br>")
+
+
+// document.write("The value of a-- is: "+ a-- +"<br>")
+// document.write("Now the value of a is: "+ a +"<br>"+"<br>")
+
+// document.write("Result:")
+// document.write("The value of a is:"+a)
+
+
+// === Chapter 6-9 (Q2) === //
+
+// var a = 2 , b = 1
+// var result = --a - --b + ++b + b--
+
+// --a = 1
+// --a - --b = 1-0
+// --a - --b + ++b = 1-0+1
+// --a - --b + ++b + b-- = 1-0+1+1
+
+// document.write("a is:"+a+"<br>")
+// document.write("b is:"+b+"<br>")
+// document.write("Result is:"+result+)
+
+
+// === Chapter 6-9 (Q3) === //
+
+// var username = prompt("Welcome!\nYour name here")
+
+
+// === Chapter 6-9 (Q4) === //
+
+// tablenum = +prompt("Enter your number")
+
+// var tablenum = 5
+// var number1to10 = 1
+// var multiplyby =1
+// document.write("Multiplication table for " + tablenum + ":<br>");
+// document.write(
+//     tablenum+"x"+number1to10++ +"="+tablenum*multiplyby++ +"<br>"+
+//     tablenum+"x"+number1to10++ +"="+tablenum*multiplyby++ +"<br>"+
+//     tablenum+"x"+number1to10++ +"="+tablenum*multiplyby++ +"<br>"+
+//     tablenum+"x"+number1to10++ +"="+tablenum*multiplyby++ +"<br>"+
+//     tablenum+"x"+number1to10++ +"="+tablenum*multiplyby++ +"<br>"+
+//     tablenum+"x"+number1to10++ +"="+tablenum*multiplyby++ +"<br>"+
+//     tablenum+"x"+number1to10++ +"="+tablenum*multiplyby++ +"<br>"+
+//     tablenum+"x"+number1to10++ +"="+tablenum*multiplyby++ +"<br>"+
+//     tablenum+"x"+number1to10++ +"="+tablenum*multiplyby++ +"<br>"+
+//     tablenum+"x"+number1to10++ +"="+tablenum*multiplyby++ +"<br>"
+// )
+
+
+// === Chapter 6-9 (Q5) === //
