e456f2448bbed79006e517406d786e019be28af0
Izma-ali <alimuhammadsameed@gmail.com>
2024-05-17 23:09:28 +0500

    chap 10-11 added

---
+++ chapter 10-11/app.js
@@ -0,0 +1,91 @@
+// === Chapter 9-11 (Q1) === //
+
+// var cityName = prompt("Enter city name")
+// if(cityName==="Karachi"){
+//     alert("Welcome to the city of lights!")
+// } 
+
+
+// === Chapter 9-11 (Q2) === //
+
+// var gender = prompt("What's your gender?")
+// if(gender==="Male"){
+//     alert("Good morning Sir!")
+// }
+// if(gender==="Female"){
+//     alert("Good morning Ma'am!")
+// }
+
+
+// === Chapter 9-11 (Q3) === //
+
+// var signalColor = prompt("Tell me the traffic signal color!")
+// if(signalColor==="red"){
+//     alert("Must stop!")
+// }
+// if(signalColor==="yellow"){
+//     alert("Ready to move!")
+// }
+// if(signalColor==="green"){
+//     alert("Move now!")
+// }
+
+
+// === Chapter 9-11 (Q4) === //
+
+// var remainingFuel = prompt("How much fuel is remaining in your car?")
+// if(remainingFuel<"0.25ltrs"){
+//     alert("Please refil the fuel in your car!")
+// }
+
+
+// === Chapter 9-11 (Q5) === //
+// (a) //
+// var a = 4; 
+// if (++a === 5){ 
+// alert("given condition for variable a is true"); 
+// }
+// output:true
+
+// (b) //
+// var b = 82; 
+// if (b++ === 83){ 
+// alert("given condition for variable b is true"); } 
+
+// output:false
+
+// (c) //
+// var c = 12;
+// if (c++ === 13) {
+//     alert("condition 1 is true");
+// }
+// if (c === 13) {
+//     alert("condition 2 is true");
+// }
+// if (++c < 14) {
+//     alert("condition 3 is true");
+// }
+// if (c === 14) {
+//     alert("condition 4 is true");
+// }
+// output:2 and 4 are true
+
+// (d) //
+// var materialCost = 20000; 
+// var laborCost = 2000; 
+// var totalCost = materialCost + laborCost; if (totalCost === laborCost + materialCost){ alert("The cost equals"); 
+// } 
+// output:true
+
+// (e) //
+// if (true){ 
+//     alert("True"); 
+//     } 
+//     if (false){ 
+//     alert("False"); 
+//     }
+    
+// (f) //
+// if("car" < "cat"){ 
+//     alert("car is smaller than cat"); }
+// output:true
