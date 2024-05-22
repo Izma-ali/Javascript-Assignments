1ee645577acb2bad4c727cc55fa4cc89267bd418
Izma-ali <alimuhammadsameed@gmail.com>
2024-05-22 07:17:36 +0500

    chap 12-13 completed

---
+++ chapter 12-13/app.js
@@ -0,0 +1,115 @@
+// === Chapter 12-13 (Q1) === //
+
+// var input = prompt("Enter any character")
+// var code = input.charCodeAt(0)
+
+// if(code>=48 && code<=57){
+//     alert("It's a number")
+// }
+// else if(code>=65 && code<=90){
+//     alert("It's uppercase letter")
+// }
+// else if(code>=97 && code<=122){
+//         alert("It's lowercase letter")
+// }
+
+
+// === Chapter 12-13 (Q2) === //
+
+// var integer1 = +prompt("Enter integer one")
+// var integer2 = +prompt("Enter integer two")
+
+// if(integer1>integer2){
+//     alert(integer1+" is greater")
+// }
+// else if(integer1<integer2){
+//     alert(integer2+" is greater")
+// }
+// else if(integer1===integer2){
+//     alert("both are equal")
+// }
+
+
+// === Chapter 12-13 (Q3) === //
+
+// var number = +prompt("Enter a number to know,whether it is positive or negative")
+
+// if(number>0){
+//     alert(number+" is positive")
+// }
+// else if(number<0){
+//     alert(number+" is negative")
+// }
+// else if(number===0){
+//     alert("It's a zero")
+// }
+
+
+// === Chapter 12-13 (Q4) === //
+
+// var character = prompt("Enter a vowel")
+
+// if(character==="a"){
+//        alert(character+" is vowel")
+// }
+// else if(character==="e"){
+//        alert(character+" is vowel")
+// }
+// else if(character==="i"){
+//        alert(character+" is vowel")
+// }
+// else if(character==="o"){
+//        alert(character+" is vowel")
+// }
+// else if(character==="u"){
+//        alert(character+" is vowel")
+// }
+// else{
+//     alert("false")
+// }
+
+
+// === Chapter 12-13 (Q5) === //
+
+// var password = "mypassword"
+// var userPassword = prompt("Enter your password")
+
+// if(!userPassword){
+//     alert("Please enter your password")
+// }
+// else if(password===userPassword){
+//     alert("Correct! The password you entered matches the original password")
+// }
+// else{
+//     alert("Incorrect password")
+// }
+
+
+// === Chapter 12-13 (Q6) === //
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+//  alert("Good day")
+// }
+// else{
+// alert("Good evening")
+// }
+
+
+// === Chapter 12-13 (Q7) === //
+
+// var time = +prompt("Enter time in 24 hours format")
+
+// if(time>="0000" && time<1200){
+//     alert("Good moning!")
+// }
+// if(time>=1200 && time<1700){
+//     alert("Good afternoon!")
+// }
+// if(time>=1700 && time<2100){
+//     alert("Good evening!")
+// }
+// if(time>=2100 && time<2359){
+//     alert("Good night!")
+// }
