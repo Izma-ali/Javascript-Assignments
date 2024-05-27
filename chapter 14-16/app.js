13f37ca58735bb637159646e8345e9e8800d0337
Izma-ali <alimuhammadsameed@gmail.com>
2024-05-27 08:23:16 +0500

    chapter 14-16 complete

---
+++ chapter 14-16/app.js
@@ -0,0 +1,124 @@
+// === Chapter 14-16 (Q1) === //
+
+// var studentNames = []
+
+
+// === Chapter 14-16 (Q2) === //
+
+// var studentNames = [{}]
+
+
+// === Chapter 14-16 (Q3) === //
+
+// var strings = ["a","b","c"]
+
+
+// === Chapter 14-16 (Q4) === //
+
+// var numbers =[1,2,3]
+
+
+// === Chapter 14-16 (Q5) === //
+
+// var boolean = ["true","false"]
+
+
+// === Chapter 14-16 (Q6) === //
+
+// var arrayMixed = ["a",1,"b",2]
+
+
+// === Chapter 14-16 (Q7) === //
+
+// var availableQualifications = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"]
+// document.write("Qualifications:"+"<br>")
+// document.write("1."+availableQualifications[0]+"<br>"+
+// "2."+availableQualifications[1]+"<br>"+
+// "3."+availableQualifications[2]+"<br>"+
+// "4."+availableQualifications[3]+"<br>"+
+// "5."+availableQualifications[4]+"<br>"+
+// "6."+availableQualifications[5]+"<br>"+
+// "7."+availableQualifications[6]+"<br>"+
+// "8."+availableQualifications[7]+"<br>"   
+// )
+
+
+// === Chapter 14-16 (Q8) === //
+
+// var studentNames = ["Izma","Sameed","Wirad"]
+// var score = [450,420,460]
+// var totalMarks = 500
+
+// document.write("Score of "+studentNames[0]+" is "+score[0]+".Percentage:"+score[0]/totalMarks*100+"%"+"<br>"+
+// "Score of "+studentNames[1]+" is "+score[1]+".Percentage:"+score[1]/totalMarks*100+"%"+"<br>"+
+// "Score of "+studentNames[2]+" is "+score[2]+".Percentage:"+score[2]/totalMarks*100+"%"+"<br>"
+// ) 
+
+
+// === Chapter 14-16 (Q9) === //
+
+// var colorNames = ["orange","red","yellow","blue","green"]
+// document.write(colorNames)
+// var color = prompt("What color you want to add in the beggining?")
+// colorNames[4]=color
+
+
+
+
+
+
+// === Chapter 14-16 (Q10) === //
+
+// var studentsScore = [320,230,480,120]
+// var orderedscore = studentsScore.sort()
+// console.log(orderedscore)
+
+
+// === Chapter 14-16 (Q11) === //
+
+// var citiesName = ["Lahore","Karachi","Islamabad","Quetta","Peshawar"]
+// var selectedCities = citiesName[1,3]
+// console.log(selectedCities)
+
+
+// === Chapter 14-16 (Q12) === //
+
+// var arr = ["This","is","my","cat"]
+// var singlestring = arr.join(" ")
+// console.log(singlestring)
+
+
+// === Chapter 14-16 (Q13) === //
+
+// var devices = []
+// devices.push("keyboard")
+// devices.push("mouse")
+// devices.push("printer")
+// devices.push("monitor")
+
+// console.log(devices)
+
+
+// === Chapter 14-16 (Q14) === //
+
+// var devices = []
+// devices.push("keyboard")
+// devices.push("mouse")
+// devices.push("printer")
+// devices.push("monitor")
+
+// var devicesReversed = devices.reverse()
+
+// console.log(devicesReversed)
+
+
+// === Chapter 14-16 (Q15) === //
+
+var phManufacturers = []
+phManufacturers.push(Apple)
+phManufacturers.push(Samsung)
+phManufacturers.push(Motorola)
+phManufacturers.push(Nokia)
+phManufacturers.push(Sony)
+phManufacturers.push(Haier)
+
