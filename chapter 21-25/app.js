2b9083afbd8605999cf2a59e77f87a65df65bba3
Izma-ali <alimuhammadsameed@gmail.com>
2024-07-01 03:39:42 +0500

    chapter 21-25 updated

---
+++ chapter 21-25/app.js
@@ -0,0 +1,92 @@
+// === Chapter 20-25 (Q1) === //
+
+// var firstName = prompt("Enter your first name") 
+// var lastName = prompt("Enter your last name") 
+
+// var fullName = firstName+lastName
+//  alert("welcome "+fullName)
+
+
+// === Chapter 20-25 (Q2) === //
+
+// var fvrtPhone = prompt("What's your favourite mobile phone model?")
+// var inputLength = fvrtPhone.length
+// document.write(fvrtPhone)
+// document.write("<br>")
+// document.write("Length of your input :"+inputLength)
+
+// === Chapter 20-25 (Q3) === //
+
+// var str = "Pakistani"
+// document.write("Index of 'n' :"+str.indexOf("n"))
+
+
+// === Chapter 20-25 (Q4) === //
+
+// var str = "Hello World"
+// document.write("Last index of 'l' :"+str.lastIndexOf("l"))
+
+
+// === Chapter 20-25 (Q5) === //
+
+// var str = "Pakistani"
+// document.write("Character at index 3 :"+str.charAt(3))
+
+
+// === Chapter 20-25 (Q6) === //
+
+// var firstName = prompt("Enter your first name") 
+// var lastName = prompt("Enter your last name") 
+
+// var fullName = firstName.concat(' ',lastName)
+// console.log(fullName);
+
+
+// === Chapter 20-25 (Q7) === //
+
+// var city = "Hyderabad"
+// var replaced = city.replace('Hyder','Islam')
+
+// document.write("After replacement :"+replaced)
+
+
+// === Chapter 20-25 (Q8) === //
+
+// var message = "Ali and Sami are best friends.They play cricket and football together."
+// var replaced = message.replace(/and/g,'&')
+// console.log(replaced);
+
+
+// === Chapter 20-25 (Q9) === //
+
+// var value = "472"
+// document.write("Value :"+value+"<br>")var
+// document.write("Type :"+typeof(value)+"<br>")
+// document.write("Value :"+value+"<br>")
+// var num = Number(value)
+// document.write("Type :"+typeof(num))
+
+
+// === Chapter 20-25 (Q10) === //
+
+// var input = prompt("Enter a dryfruit name")
+// document.write(input.toUpperCase())
+
+
+// === Chapter 20-25 (Q11) === //
+
+// var input = prompt("Enter something name")
+// var titleCase = input.slice(0,1).toUpperCase()+input.slice(1).toLowerCase()
+// console.log(titleCase);
+
+
+// === Chapter 20-25 (Q12) === //
+
+// var num = 35.36
+// var converted  = num.toString().replace(".","")
+
+// document.write(converted)
+
+
+// === Chapter 20-25 (Q13) === //
+
