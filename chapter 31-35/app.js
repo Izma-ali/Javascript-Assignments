a98912c29c35e8cbc087df0d7690aedcfef1e0d3
Izma-ali <alimuhammadsameed@gmail.com>
2024-07-08 00:42:11 +0500

    chapter 31-35 added

---
+++ chapter 31-35/app.js
@@ -0,0 +1,21 @@
+// === Chapter 31-35 (Q1) === //
+
+// var rightnow = new Date()
+// console.log(rightnow);
+
+
+// === Chapter 31-35 (Q2) === //
+
+// var months = ['january','february','march','april','may','june','july','august','september','october','november','december']
+// var now = new Date()
+// var currentMonth = now.getMonth()
+// var nameOfMonth = months[currentMonth]
+// alert(nameOfMonth)
+
+
+// === Chapter 31-35 (Q3) === //
+// var days = ['sun','mon','tue','wed','thur','fri','sat']
+// var now = new Date()
+// var currentday = now.getDay()
+// var nameOfday = days[currentday]
+// alert("Today is "+nameOfday)
