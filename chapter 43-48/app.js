d23c55554720568028e058c0ba098237d0cd06be
Izma-ali <alimuhammadsameed@gmail.com>
2024-08-02 20:35:12 +0500

    chapter 43-48 added

---
+++ chapter 43-48/app.js
@@ -0,0 +1,12 @@
+// === Chapter 43-48 (Q1) === //
+
+// function greet() {
+//     alert('Hi')
+// }
+
+
+// === Chapter 43-48 (Q2) === //
+
+function customer() {
+    alert('Thankyou for purchasing phone from us')
+}
