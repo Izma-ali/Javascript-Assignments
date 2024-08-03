1d480ff2683f620f4742c05c120959c5216caeb7
Izma-ali <alimuhammadsameed@gmail.com>
2024-08-03 20:02:30 +0500

    chapter 43-48 completedde

--- chapter 43-48/app.js
+++ chapter 43-48/app.js
@@ -7,6 +7,19 @@
 
 // === Chapter 43-48 (Q2) === //
 
-function customer() {
-    alert('Thankyou for purchasing phone from us')
-}
+// function customer() {
+//     alert('Thankyou for purchasing phone from us')
+// }
+
+// === Chapter 43-48 (Q5) === //
+
+// var counter=0
+// function increase() {
+//     document.getElementById('counter').innerText =counter++
+// }
+// function decrease(param) {
+//     if (counter<0) {
+//         counter=0
+//     }
+//     document.getElementById('counter').innerText =counter--
+// }
